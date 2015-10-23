4d40b98348b8831404bc86b9b34810e3acbec416
ntisod-Filip-Kapotra <filip.kapotra.te13@sodertalje.ntig.se>
2015-10-23 13:00:36 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+from trafiklab.sl import sites,trains,buses
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+
+    return render_template("index.html", departures=trains(9521), departures2=buses(9521))
+
+#hack för att hitta sitid
+@app.route('/sites')
+def sitessearch():
+
+    sitelist = sites(("Sodertalje hamn"))
+
+    return    str(sitelist)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ trafiklab/sl.py
@@ -0,0 +1,32 @@
+import requests
+import datetime
+
+from settings import key_plats,key_real
+
+def sites(searchstring):
+    url = "http://api.sl.se/api2/typeahead.json?key={key}&searchstring={searchstring}"
+
+    # download the page from trafiklab
+    resp = requests.get(url.format(key=key_plats,searchstring = searchstring))
+
+
+
+    return  resp.json()['ResponseData']
+
+def departures(siteid):
+
+    url = "http://api.sl.se/api2/realtimedepartures.json?key={key}&siteid={siteid}&timewindow=60"
+
+    resp = requests.get(url.format(key=key_real,siteid = siteid))
+
+
+    return resp.json()['ResponseData']
+
+
+def trains(siteid):
+    trainlist=[]
+    return   departures(siteid)['Trains']
+
+def buses(siteid):
+    buslist=[]
+    return   departures(siteid)['Buses']
